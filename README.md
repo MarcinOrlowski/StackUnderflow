6921fa4da6be85ceb37daa37f7c7c046b51cbb13
Marcin Orlowski <marcin.orlowski@webnet.pl>
2015-08-10 23:12:45 +0200

    Initial import of v0.2

---
+++ README.md
@@ -0,0 +1,52 @@
+### StackUnderflow
+
+ StackUnderflow is Greasemonkey/Tampermonkey (or compatible) user script that aims to improve your experience with StackOverflow.com site. As for now:
+
+  - user blacklist
+    - warning on question asked by blacklisted user
+  - favorite users
+    - notice on question asked by favourite user
+  - warnings on low reputation user question
+  - and more...
+
+ You should easily notice additiona icons appearing next to details of author of each question or answer:
+
+![New icons](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/docs_icons.png)
+
+ 1. indicates whenever user is on your blacklist ![blacklisted](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-blacklisted-on.png) or not ![not blacklisted](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-blacklisted-off.png)
+ 2. indicates whenever you consider this user as your favourite (starred) one. Tap this icon to toggle the state. ![blacklisted](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-favourite-on.png) or just regular one ![regular user](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-favourite-off.png). Tap this icon to star/unstar this user.
+
+All questions asked by either your favourite users will be additionally marked:
+
+![starred user question](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/docs_banner_starred_question.png)
+
+ You also see similar notice for questions placed by blacklisted users:
+
+![blacklisted user question](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/docs_banner_blacklisted_user_question.png)
+
+ If question is already answered, similar notice will be shown and accepted answer will be additnionaly highlighted:
+
+![accepted answer highlight](https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/docs_accepted_answer.png)
+
+### Installation
+ To enjoy StackUnderflow you need to
+
+  - install user script host extension:
+    - [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome and compatible browsers) or
+    - [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (Firefox)
+  - install StackUnderflow script by [clicking here](https://github.com/MarcinOrlowski/StackUnderflow/raw/master/stackunderflow.user.js) - script shall be automatically installed and ready to use.
+
+### Configuration
+ As for now configuration features are limited and there's no configuration dialog yet. Some features are however configurable directly in the script, so once installed, open script for edit and you can find all `cfg_....` parameters at the beginig of the script. Tweak as you need, save and reload target StackOverflow page.
+
+### Future
+I created this script with my needs in mind, however I hope you find it useful too. If you find a bug, feel free to report it or send pull request. If you miss a feature, use GitHub "issues" to let me know.
+
+Marcin Orlowski
+
+### Changelog
+ - v0.2 (2015-08-10)
+  - Added action icons
+  
+ - v0.1 (2015-08-09)
+  - Initial release
---
+++ stackunderflow.user.js
@@ -0,0 +1,322 @@
+// ==UserScript==
+// @name         StackUnderflow
+// @namespace    http://webnetmobile.com/
+// @version      0.1
+// @description  Brings user blacklisting, favouries and other goodies to StackOverflow.com
+// @author       Marcin Orlowski
+// @match        https://stackoverflow.com/questions/*
+// @require      https://code.jquery.com/jquery-latest.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// ==/UserScript==
+// The @grant GM_addStyle directive is needed to work around a major design change introduced in GM 1.0, restores the sandbox.
+
+//--[ THIS IS CONFIG. CHANGE AS YOU WANT ]---------------------------------------------------
+
+// true/false: when true (default) you will see banner on top of the screen when question you are reading is already answered and answer is accepted by OP
+var cfg_enableAcceptedAnswerWarning = true;
+
+// true/false: when true (default) accepted answer (if any) will be additionally highlighted
+var cfg_highlightAcceptedAnswer = true;
+
+// true/false: when true (default) you will seel warning if question you are reading is asked by user with reputation lower than cfg_questionPosterReputationThreshold value
+var cfg_enableLowReputationWarning = true;
+var cfg_questionPosterReputationThreshold = 500;
+
+// true/false: when true (default) you will see warning when you are reading a question asked by user you already blacklisted
+var cfg_enablePostedByBlacklistedUserWarning = true;
+
+// true/false: when true (default) yo uwill see information when you are reading question asked by user you got on your favourite user list
+var cfg_enablePostedByFavouriteUserWarning = true;
+
+var cfg_userBlacklistedOnUrl = "https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-blacklisted-on.png";
+var cfg_userBlacklistedOffUrl = "https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-blacklisted-off.png";
+var cfg_userFavouriteOnUrl = "https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-favourite-on.png";
+var cfg_userFavouriteOffUrl = "https://raw.githubusercontent.com/MarcinOrlowski/StackUnderflow/master/img/user-favourite-off.png";
+
+
+//--[ DO NOT ALTER ANYTHING BELOW ]---------------------------------------------------
+
+var wbn_postedByBlacklistedUserBannerSet = false;
+var wbn_questionHasAcceptedAnswerBannerSet = false;
+var wbn_lowReputationWarningBannerSet = false;
+var wbn_postedByFavouriteUserBannerSet = false;
+
+var wbn_blacklistedEntryPrefix = "blacklisted_";
+var wbn_favouriteEntryPrefix = "favourite_";
+
+//-----------------------------------------------------
+
+var myId = $(".topbar-links > a").attr("href").split("/")[2];
+
+var posterRoot = $(".post-signature.owner");
+var posterName = posterRoot.find(".user-details > a").text();
+var posterId = posterRoot.find(".user-details > a").attr("href").split("/")[2];
+
+var posterReputation = posterRoot.find(".reputation-score").attr("title").split(" ")[2];
+// poster reputation missing in title of for some questions (most likely SO bug):
+// https://stackoverflow.com/questions/31902812/  https://stackoverflow.com/questions/5937121/
+if (posterReputation == "") {
+    posterReputation = posterRoot.find(".reputation-score").text().replace(" ","").replace(",", "");
+    // check if we got only digits here. If not, we we assume we have "k" suffix
+    var reg = new RegExp("[0-9]");
+    if (!reg.test(posterReputation)) {
+        posterReputation = posterReputation.replace("k","") * 1000;
+    }
+}
+
+//$('body').append('<div id="SOINFO">myId: ' + myId + "<br/>pName: " + posterName + "<br/>pId: " + posterId + "<br/>pRep: " + posterReputation + "<br/>");
+//$("#SOINFO").css("position", "fixed").css("background", "red").css("top", 0).css("left", 0);
+
+addStyles();
+updateDisplay();
+
+//-----------------------------------------------------
+
+function clickToggleBlacklist(event) {
+    blacklistToggle(event.data.userId);
+    updateDisplay();
+}
+
+function clickToggleFavourite(event) {
+    favouriteToggle(event.data.userId);
+    updateDisplay();
+}
+
+function updateDisplay() {
+
+    var isPosterFavourite = isFavourite(posterId);
+    var isPosterBlacklisted = isBlacklisted(posterId);
+
+    // update banners
+    var hasAcceptedAnswer = $("#answers .answer.accepted-answer")[0];
+
+    // plant banners
+    if (!wbn_questionHasAcceptedAnswerBannerSet) {
+        if (hasAcceptedAnswer) {
+            var hasAnswerBanner = '<div id="wbn_questionHasAcceptedAnswer" class="wbn_banner wbn_okBanner">Question has accepted answer</div>';
+            $(hasAnswerBanner).insertBefore("#question-header");
+            $(hasAnswerBanner).insertBefore("#post-editor"); 
+        
+            if (cfg_highlightAcceptedAnswer) {
+                $("#answers .answer.accepted-answer").addClass("wbn_acceptedAnswerHighlight");
+            }
+
+            wbn_questionHasAcceptedAnswerBannerSet = true;
+        }
+    }
+
+    if (!wbn_postedByBlacklistedUserBannerSet) {
+        var blacklistedUserPostBanner = '<div class="wbn_postedByBlacklistedUserBanner wbn_banner wbn_blacklistedBanner wbn_hidden"><img class="wbn_userActionIconSmall" src="' + cfg_userBlacklistedOnUrl + '"> Question asked by blacklisted user</div>';
+        $(blacklistedUserPostBanner).insertBefore("#question-header");
+        $(blacklistedUserPostBanner).insertBefore("#post-editor"); 
+
+        wbn_postedByBlacklistedUserBannerSet = true;
+    }
+
+    if (!wbn_postedByFavouriteUserBannerSet) {
+        var favouriteUserPostBanner = '<div class="wbn_postedByFavouriteUserBanner wbn_banner wbn_favouriteBanner wbn_hidden"><img class="wbn_userActionIconSmall" src="' + cfg_userFavouriteOnUrl + '"> Question asked by favourited user</div>';
+        $(favouriteUserPostBanner).insertBefore("#question-header");
+
+        wbn_postedByFavouriteUserBannerSet = true;
+    }
+
+    if (!wbn_lowReputationWarningBannerSet) {
+        var posterReputationLow = '<div class="wbn_lowReputationBanner wbn_banner wbn_warningBanner wbn_hidden">Poster reputation is low</div>';
+        $(posterReputationLow).insertBefore("#question-header");
+        $(posterReputationLow).insertBefore("#post-editor"); 
+        
+        wbn_lowReputationWarningBannerSet = true;
+    }
+    
+    // show/hide banners
+    if (cfg_enablePostedByBlacklistedUserWarning) {
+        var blBanner = $(".wbn_postedByBlacklistedUserBanner");
+        if (isPosterBlacklisted) {
+            blBanner.fadeIn(500);
+        } else {
+            blBanner.hide();
+        }
+    }
+    
+    if (cfg_enablePostedByFavouriteUserWarning) {
+        var favBanner = $(".wbn_postedByFavouriteUserBanner");
+        if (isPosterFavourite) {
+            favBanner.fadeIn(500);
+        } else {
+            favBanner.hide();
+        }
+    }
+
+    if ((!isPosterFavourite) && (!isPosterBlacklisted)) {
+        if ((!hasAcceptedAnswer) && (cfg_enableLowReputationWarning) && (posterReputation < cfg_questionPosterReputationThreshold)) {
+            $(".wbn_lowReputationBanner").fadeIn(500);
+        }
+    } else {
+        $(".wbn_lowReputationBanner").hide();
+    }
+        
+
+    
+    // update links
+    $(".post-signature > .user-info").each(function(index){updateUserLinksRaw(index,$(this));});
+    
+}
+
+function updateUserLinksRaw(index, element) {
+    var userId = element.find(".user-details > a").attr("href");//.split("/")[2];
+    if (userId !== undefined) {
+        userId = userId.split("/")[2];
+        
+        if (userId != myId ) {
+            var isFav = isFavourite(userId);
+            var isBl = isBlacklisted(userId);
+            
+            var actionId = "wbn_action_user_" + userId;
+            element.after('<div id="' + actionId + '"></div>');
+
+            // blacklist
+            var blacklistId = "wbn_blacklist_" + userId;      
+            var blLabel = isBl ? "Click to remove this user from blacklist" : "Click to blacklist this user";
+            var blIconUrl = isBl ? cfg_userBlacklistedOnUrl : cfg_userBlacklistedOffUrl;
+            if ($("#" + blacklistId)[0]) {
+                var blIcon = $("#" + blacklistId + " > img");
+                blIcon.attr("src", blIconUrl);
+                blIcon.attr("alt", blLabel);
+                blIcon.attr("title", blLabel);
+            } else {
+                $("#" + actionId).append('<a id="' + blacklistId + '"><img class="wbn_userActionIcon" title="' + blLabel + '" alt="' + blLabel + '" src="' + blIconUrl + '"></a>');
+                $("#" + blacklistId).click({userId: userId}, clickToggleBlacklist);
+            }
+
+            var blContainer = $("#" + blacklistId);
+            if (!isFav) {
+                blContainer.css("visibility", "visible");
+            } else {
+                blContainer.css("visibility", "hidden");
+            }
+
+                        // favourite
+            var favId = "wbn_favourite_" + userId;
+            var favLabel = isFav ? "Click to remove from favourites" : "Click to mark user as your favourite";
+            var favIconUrl = isFav ? cfg_userFavouriteOnUrl : cfg_userFavouriteOffUrl;
+            if ($("#" + favId)[0]) {
+                var favIcon = $("#" + favId + " > img");
+                favIcon.attr("src", favIconUrl);
+                favIcon.attr("alt", favLabel);
+                favIcon.attr("title", favLabel);
+            } else {
+                $("#" + actionId).append('<a id="' + favId + '"><img class="wbn_userActionIcon" title="' + favLabel + '" alt="' + favLabel + '" src="' + favIconUrl + '"></a>');
+                $("#" + favId).click({userId: userId}, clickToggleFavourite);
+            }
+            
+            var favContainer = $("#" + favId);
+            if (!isBl) {
+                favContainer.css("visibility", "visible");
+            } else {
+                favContainer.css("visibility", "hidden");
+            }
+
+
+        }
+    }
+}
+
+// ----------------------------------------------------------------------
+
+function isBlacklisted(userId) {
+    return GM_getValue(wbn_blacklistedEntryPrefix + userId, false);
+}
+function blacklistToggle(userId) {
+    toggleUserFlag(wbn_blacklistedEntryPrefix + userId);
+}
+
+function isFavourite(userId) {
+    return GM_getValue(wbn_favouriteEntryPrefix + userId, false);
+}
+function favouriteToggle(userId) {
+    toggleUserFlag(wbn_favouriteEntryPrefix + userId);
+}
+
+function toggleUserFlag(key) {
+    var current = GM_getValue(key, false);
+    GM_deleteValue(key);
+    GM_setValue(key, (!current));
+}
+
+function multilineStr (dummyFunc) {
+    var str = dummyFunc.toString ();
+    str     = str.replace (/^[^\/]+\/\*!?/, '') // Strip function () { /*!
+            .replace (/\s*\*\/\s*\}\s*$/, '')   // Strip */ }
+            .replace (/\/\/.+$/gm, '') // Double-slash comments wreck CSS. Strip them.
+            ;
+    return str;
+}
+
+//--- Style our newly added elements using CSS.
+function addStyles() {
+GM_addStyle ( multilineStr ( function () {/*!
+    .wbn_hidden {
+        display: none;
+    }
+    
+    .wbn_banner {
+        padding: 10px;
+        text-align: center;
+        font-size: 16px;
+    }
+
+    .wbn_okBanner {
+        background: #E9FCF6;
+        color: black;
+    }
+    
+    .wbn_warningBanner {
+        background: #CB5555;
+        color: white;
+    }
+    
+    .wbn_favouriteBanner {
+        background: #FFDD00;
+        color: black;
+    }
+
+    .wbn_blacklistedBanner {
+        background: #333333;
+        color: white;
+    }
+
+    .wbn_acceptedAnswerHighlight {
+        background: #E9FCF6;
+    }
+    
+    .wbn_blacklistLink {
+        background: #000000;
+        color: white;
+    }
+    
+    .wbn_favouriteLink {
+        background: #FFDD00;
+        color: black;
+    }
+    
+    .wbn_userActionIcon {
+        width: 24px;
+        height: 24px;
+        border: 0;
+        padding: 2px;
+        vertical-align: middle;
+    }
+
+    .wbn_userActionIconSmall {
+        width: 18px;
+        height: 18px;
+        border: 0;
+        padding: 0px;
+        vertical-align: middle;
+    }
+
+*/} ) );
+}
